95e4868f54a4e1d9e1c7171e68b8b9649da8852e
Cody De Arkland <codydearkland@gmail.com>
2022-04-26 23:29:11 -0700

    adding assets

--- ld-k8s-frontend/pages/index.js
+++ ld-k8s-frontend/pages/index.js
@@ -8,7 +8,7 @@ import QRCode from "../components/qrCode";
 import Banner from "../components/banner.js";
 
 export default function Home() {
-  const { userLogin, apiConfig, qrCode, brandImage } = useFlags();
+  const { newText, userLogin, apiConfig, brandImage } = useFlags();
   return (
     <div className="h-screen bg-ld-ls-wide bg-no-repeat bg-center bg-cover">
       <Head>
@@ -74,7 +74,7 @@ export default function Home() {
           {userLogin ? (
             <Loginbox userLogin={userLogin} />
           ) : (
-            <Banner userLogin={userLogin} />
+            <Banner />
           )}
         </div>
         {apiConfig ? (
