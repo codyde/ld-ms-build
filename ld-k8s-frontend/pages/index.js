b9a87d1da1f4404f902911d17f0c9e0a5227ed6e
Cody De Arkland <codydearkland@gmail.com>
2022-05-12 22:56:29 -0700

    Adding QR code component back oin

--- ld-k8s-frontend/pages/index.js
+++ ld-k8s-frontend/pages/index.js
@@ -51,6 +51,11 @@ export default function Home() {
         }}
       />
       <main className="h-screen grid grid-cols-4 grid-rows-3">
+      {qrCode ? (
+          <div className="grid col-span-4 row-start-3 my-8 lg:row-start-1 lg:col-span-1 lg:col-start-1 justify-center items-center px-8">
+            <QRCode />
+          </div>
+        ) : null}
         {brandImage ? (
           <div className="grid col-span-4 row-start-3 lg:row-start-2 lg:col-span-1 lg:col-start-1 justify-center items-center px-8">
             <img
